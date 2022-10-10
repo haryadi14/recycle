b8e02bc6412e8c396b387f96fb3a0328c78f61ef
haryadi <haryadiyusuf61@gmail.com>
2022-10-10 19:35:39 +0700

    projek recycle

---
+++ app/src/main/java/com/example/haryadiy/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.haryadiy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    AdapterData adapterData;
+    List<String> listData;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rvData);
+        listData = new ArrayList<>();
+        for (int i=0; i<10; i++){
+            listData.add("data ke"+i);
+
+    }
+        linearLayoutManager  = new LinearLayoutManager(this,linearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapterData = new AdapterData(this,listData);
+        recyclerView.setAdapter(adapterData);
+        adapterData.notifyDataSetChanged();
+    }
+}
